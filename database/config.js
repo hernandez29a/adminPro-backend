58bbf601193d9afd03e249bbe0cda938f8dcd4a5
Artisan <soporte.gobernaciondelara@gmail.com>
2021-02-05 11:04:09 -0400

    Colocacion de ruta de la bd local

--- database/config.js
+++ database/config.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
+//Conexion a la Base de datos
+/*mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos:\x1b[32m%s\x1b[0m ', 'En Línea');
+
+})*/
+
 
 const dbConnection = async() => {
 
